d50e1238c567d705d20325e3a175394a1c97e898
Mckenna Preston <Mckenna_Preston1@baylor.edu>
2022-03-02 15:52:37 -0600

    midterm

--- CourseProgram.py
+++ CourseProgram.py
@@ -7,7 +7,7 @@ You have been given partial code. The objective is to reproduce the output as sh
 4) Take note that student 'Joanne' cannot register since there are only 4 seats in the course and
    and the output should reflect that as shown in the picture.
 """
-from CourseClass import RegisterClass as r
+
 import CourseClass as c
 
 
@@ -19,13 +19,16 @@ def main():
     status = "open"
     students = ["John", "James", "Jill", "Jack", "Joanne"]
 
+    for student in students:
+        print(student)
+
     MyCourse = c.Course("MIS 4322 - Advanced Python", "250309", 4, "open")
 
-    MyStudent1 = c.Course("John")
-    MyStudent2 = c.Course("James")
-    MyStudent3 = c, Course("Jill")
-    MyStudent4 = c.Couse("Jack")
-    MyStudent5 = c.Course("Joanne")
+    MyStudent1 = c.Course("John", crn, seats, status)
+    MyStudent2 = c.Course("James", crn, seats, status)
+    MyStudent3 = c.Course("Jill", crn, seats, status)
+    MyStudent4 = c.Course("Jack", crn, seats, status)
+    MyStudent5 = c.Course("Joanne", crn, seats, status)
 
     total_seats = 4
 
@@ -33,6 +36,36 @@ def main():
     print("Course Name: ", MyCourse.get_name())
     print("CRN: ", MyCourse.get_crn())
     print("Seats left: ", MyCourse.get_seats())
+    print()
+
+    print("Student Name: ", MyStudent2.get_name())
+    print("Course Name: ", MyCourse.get_name())
+    print("CRN: ", MyCourse.get_crn())
+    print("Seats left: ", MyCourse.get_seats())
+    print()
+
+    print("Student Name: ", MyStudent3.get_name())
+    print("Course Name: ", MyCourse.get_name())
+    print("CRN: ", MyCourse.get_crn())
+    print("Seats left: ", MyCourse.get_seats())
+    print()
+
+    print("Student Name: ", MyStudent4.get_name())
+    print("Course Name: ", MyCourse.get_name())
+    print("CRN: ", MyCourse.get_crn())
+    print("Seats left: ", MyCourse.get_seats())
+    print()
+
+    if MyCourse.get_crn() == MyStudent1.get_crn():
+        total_seats -= 1
+    if MyCourse.get_crn() == MyStudent2.get_crn():
+        total_seats -= 1
+    if MyCourse.get_crn() == MyStudent3.get_crn():
+        total_seats -= 1
+    if MyCourse.get_crn() == MyStudent4.get_crn():
+        total_seats -= 1
+    if MyCourse.get_crn() == MyStudent5.get_crn():
+        total_seats -= 1
 
     if seats == 0:
         print(
